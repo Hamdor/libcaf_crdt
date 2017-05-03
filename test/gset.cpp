41fa526ac3fcc3a4daf6d61dfa05bd0aa9809731
Marian Triebe <m.triebe@live.de>
2017-05-03 14:25:28 +0200

    Enable gset test again

--- test/gset.cpp
+++ test/gset.cpp
@@ -20,146 +20,24 @@
 #define CAF_SUITE gset
 #include "caf/test/unit_test.hpp"
 
-#include "caf/all.hpp"
-#include "caf/io/all.hpp"
 #include "caf/crdt/all.hpp"
-/*
-using namespace caf;
-using namespace caf::crdt;
 
-namespace {
+using namespace caf::crdt::types;
 
-constexpr int buddy_count = 5;
-constexpr int runs        = 10;
-
-constexpr int assumed_msgs = buddy_count * runs;
-
-uri u{"gset<int>://rand"};
-
-using request_atom = atom_constant<atom("req")>;
-using verify_atom = atom_constant<atom("verify")>;
-using checked_atom = atom_constant<atom("checked")>;
-
-using testee_type = typed_actor<
-  replies_to<tick_atom>::with<int>,
-  replies_to<request_atom>::with<types::gset<int>>
->::extend_with<notifiable<types::gset<int>>>;
-
-struct testee : public testee_type::base {
-  testee(actor_config& cfg)
-    : testee_type::base(cfg) {
-    system().replicator().subscribe<types::gset<int>>(u, this);
-  }
-
-protected:
-  typename testee_type::behavior_type make_behavior() override {
-    return {
-      [&](initial_atom, types::gset<int>& state) {
-        state_ = std::move(state);
-      },
-      [&](notify_atom, const types::gset<int>::transaction_t& operations) {
-        state_.apply(operations);
-      },
-      // --- Generate new data
-      [&](tick_atom) {
-        auto rnd = std::rand();
-        state_.insert(rnd);
-        return rnd;
-      },
-      [&](request_atom) {
-        return state_;
-      }
-    };
-  }
-
-private:
-  types::gset<int> state_;
-};
-
-struct verifier : public event_based_actor {
-  verifier(actor_config& cfg) : event_based_actor(cfg) {
-    // nop
-  }
-
-protected:
-  virtual behavior make_behavior() override {
-    // Spawn testees
-    for (int i = 0; i < buddy_count; ++i)
-      buddies_.emplace(spawn<testee>());
-    // Send initial tasks
-    for (int i = 0; i < runs; ++i)
-      for (auto& buddy : buddies_)
-        send(buddy, tick_atom::value);
-    return {
-      // A testee inserted a value n
-      [&](int n) {
-        values_.insert(n);
-        // Let some time pass to give everyone the chance to apply pending
-        // updates
-        if (++msgs == assumed_msgs)
-          delayed_send(this, std::chrono::seconds(2), verify_atom::value);
-      },
-      // trigger testees to send their state back
-      [&](verify_atom) {
-        for (auto& buddy : buddies_)
-          send(buddy, request_atom::value);
-      },
-      // check testees states if they are equal
-      [&](const types::gset<int>& check) {
-        CAF_CHECK(check.equal(values_));
-        send(this, checked_atom::value);
-      },
-      // if all are checked, just exit all actors...
-      [&](checked_atom) {
-        if (++checked_ == buddy_count) {
-          for (auto& buddy : buddies_)
-            anon_send_exit(buddy, exit_reason::user_shutdown);
-          quit();
-        }
-      }
-    };
-  }
-
-private:
-  int checked_ = 0;
-  int msgs = 0;
-  std::set<int> values_;
-  std::set<testee_type> buddies_;
-};
-
-void test_init(const std::set<int>& init_val,
-               const std::set<int>& def_val,
-               const std::set<int>& assumed) {
-  types::gset<int> init{std::move(init_val)};
-  types::gset<int> def{std::move(def_val)};
-  def = std::move(init);
-  for (auto& e : assumed)
-    CAF_CHECK(def.element_of(e));
+void test_merge(const std::set<int>& lhs_, const std::set<int>& rhs_,
+                const std::set<int>& assumed_delta_) {
+  gset<int> lhs, rhs;
+  lhs.subset_insert(lhs_);
+  rhs.subset_insert(rhs_);
+  auto delta = lhs.merge(rhs);
+  for (auto& i : assumed_delta_)
+    CAF_CHECK(delta.element_of(i));
+  CAF_CHECK(delta.size() == assumed_delta_.size());
 }
 
-} // namespace <anonymous>
-
-// This test let some actors work with the replica data
-CAF_TEST(run_inserts) {
-  crdt_config cfg{};
-  cfg.add_crdt<types::gset<int>>("gset<int>")
-     .add_message_type<std::set<int>>("set_int")
-     .load<io::middleman>();
-  actor_system system{cfg};
-  system.spawn<verifier>();
+CAF_TEST(merge) {
+  test_merge({1,2,3,4}, {5,6,7,8}, {5,6,7,8});
+  test_merge({}, {5,6,7,8}, {5,6,7,8});
+  test_merge({1,2,3,4}, {}, {});
+  test_merge({1,2,3,4}, {1,2,5}, {5});
 }
-
-// Tests the initialization process of crdt instances
-CAF_TEST(move1) {
-  test_init({1},{2},{1,2});
-}
-
-// Tests the initialization process of crdt instances
-CAF_TEST(move2) {
-  test_init({1},{},{1});
-}
-
-// Tests the initialization process of crdt instances
-CAF_TEST(move3) {
-  test_init({},{1},{1});
-}*/
